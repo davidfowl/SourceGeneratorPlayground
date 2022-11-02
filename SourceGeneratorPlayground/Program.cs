eddbab8bc9b8a0a62588fc1b46917730d240e810
David Fowler <davidfowl@gmail.com>
2022-11-01 22:50:40 -0700

    Changes

--- SourceGeneratorPlayground/Program.cs
+++ SourceGeneratorPlayground/Program.cs
@@ -1,46 +1,16 @@
 using System.ComponentModel.DataAnnotations;
-using System.Globalization;
-using System.Numerics;
-using System.Reflection;
-using Microsoft.AspNetCore.SignalR;
 
-//foreach (var t in typeof(IHubCallerClients<>).GetInterfaces().Concat(new[] { typeof(IHubCallerClients<>) }))
-//{
-//    Console.WriteLine(t);
-//    DumpMethods(t);
-//}
+var builder = WebApplication.CreateBuilder(args);
 
-DumpMethods(typeof(ChatHub));
+var app = builder.Build();
+
+app.MapPost("/posts", (Post p) => Results.Ok(p));
 
 
-static void DumpMethods(Type? t)
+app.Run();
+
+struct Post
 {
-    foreach (var p in t.GetMethods(BindingFlags.Public | BindingFlags.Instance))
-    {
-        // Console.WriteLine(p.DeclaringType + " and " + p.ReflectedType);
-        Console.WriteLine(p.Name);
-        Console.WriteLine(p.GetType().GetProperty("BindingFlags", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(p, null));
-    }
+    [Required]
+    public string Title { get; set; }
 }
-
-Console.WriteLine("Done");
-
-class Foo : Bar
-{
-    // public override int X => base.X;
-}
-
-class Bar
-{
-    public virtual int X { get; }
-}
-//var builder = WebApplication.CreateBuilder(args);
-//builder.Services.AddSignalR();
-//var app = builder.Build();
-
-//app.MapGet("/", () => "Hello World!");
-
-//app.MapHub<ChatHub>("/chat");
-
-//app.Run();
-
